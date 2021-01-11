8ea635b145e562f045c46b34f655406138fd31a1
C. L <amautacelt@gmail.com>
2021-01-11 00:46:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -7,10 +7,7 @@ def find_item_by_name_in_collection(name, collection)
   
   
   collection.each do |key|
-    if key.any?
-      # binding.pry
-    end
-  end
+end
 end
  
 def consolidate_cart(cart)
