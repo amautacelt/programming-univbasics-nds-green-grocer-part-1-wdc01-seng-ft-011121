30b495e71cd924025ca392c1278b728da601fffe
C. L <amautacelt@gmail.com>
2021-01-11 01:37:54 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -22,19 +22,7 @@ def consolidate_cart(cart)
   
   
   def consolidate_cart(cart)
-    output = {}
-  cart.each do |item|
-    item_name = item.keys[0]
-    if output[item_name]
-      output[item_name][:count] += 1 
-    else
-      output[item_name] = item[item_name]
-      output[item_name][:count] = 1 
-    end
-  end
-  output
-end
-
+    
 end
 
 
