cca33ab9276937d9e25c93e7b8569a65ccc76506
C. L <amautacelt@gmail.com>
2021-01-11 01:36:46 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -21,17 +21,19 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   
   
-  def consolidate_cart(cart) 
-  new_cart = {} 
-  cart.each do |items_array| 
-    items_array.each do |item, attribute_hash| 
-      new_cart[item] ||= attribute_hash 
-      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
-      new_cart[item][:count] = 1 
-    end 
-  end 
-  new_cart 
+  def consolidate_cart(cart)
+    output = {}
+  cart.each do |item|
+    item_name = item.keys[0]
+    if output[item_name]
+      output[item_name][:count] += 1 
+    else
+      output[item_name] = item[item_name]
+      output[item_name][:count] = 1 
+    end
   end
+  output
+end
 
 end
 
